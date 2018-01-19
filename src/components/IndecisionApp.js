e6a157ce2fdf53c61e317d98950fcc62d3b8c80d
yapi <yapi.tw@gmail.com>
2018-01-20 03:49:56 +0800

    Lecture 8 complete with scss styles

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -8,12 +8,14 @@ import OptionModal from './OptionModal';
 export default class IndecisionApp extends React.Component {
   state = {
     options: [],
-    selectedOption: undefined
+    selectedOption: undefined,
   };
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum]
-    this.setState(() => ({ selectedOption: option }));
+    this.setState(() => ({
+      selectedOption: option,
+    }));
   };
   handleDeleteOptions = () => {
     this.setState( () => ({ options: [] }));
@@ -61,27 +63,31 @@ export default class IndecisionApp extends React.Component {
   }
 
   render() {
-    const subtitle = 'Put your life in hands of a computer';
+    const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
         <Header subtitle={subtitle}/>
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions = {this.handleDeleteOptions}
-          handleDeleteOption = {this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal
-          selectedOption={this.state.selectedOption}
-          handleClearSelectedOption={this.handleClearSelectedOption}
-        />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              handleDeleteOptions = {this.handleDeleteOptions}
+              handleDeleteOption = {this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+            <OptionModal
+              selectedOption={this.state.selectedOption}
+              handleClearSelectedOption={this.handleClearSelectedOption}
+            />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -4,12 +4,14 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
   <Modal
     isOpen={!!props.selectedOption}
-    contentLabel="Selected Option"
     onRequestClose={props.handleClearSelectedOption}
+    contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button modal__button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
 );
 
